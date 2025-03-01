aa800aafc34f3f608e12b579e7d35c9240a8d3bd
Your Name <your.email@example.com>
2025-03-01 13:54:52 +0800

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     domains: ['placehold.co', 'images.unsplash.com'],
+    unoptimized: true,
   },
+  basePath: '/Shopify_test',
 }
 
 module.exports = nextConfig 
